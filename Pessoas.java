8e561014b6fd26433cbfb8f00192ff30ac72a4b2
yata64 <joaomarioys@gmail.com>
2026-05-18 22:27:26 -0300

    Criação de Classes
    Foi criado as classes iniciais do projeto.

---
+++ Pessoas.java
@@ -0,0 +1,26 @@
+public abstract class Pessoas{
+    private String nome;
+    private String cpf;
+
+    public Pessoas(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
+
---
+++ Produtos.java
@@ -0,0 +1,11 @@
+public class Produtos {
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    public Produtos(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+}   
